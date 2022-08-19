c4d6e64b6bca9951ffc4e6118e9c3e8d26e04b63
AdalidBA <ricardo.adalidab19@gmial.com>
2022-08-19 14:35:39 -0500

    tareas ejercicio 6 1

--- Cursos Python/Ejercicio6_1.py
+++ Cursos Python/Ejercicio6_1.py
@@ -1,11 +1,23 @@
 class vehiculo:
-    color="verde"
-    ruedas=4
-    puertas=5
+    def __init__(self,color,ruedas,puertas):
+        self.color=color
+        self.ruedas=ruedas
+        self.puertas=puertas
+    def __str__(self):
+        return "{} es el color, {} es la cantidad de ruedas y tiene {} puertas".format(self.color,self.ruedas,self.puertas)
+    
     
 class coche(vehiculo):
-    velocidad=120
-    cilindrada=4
+    def __init__(self, color, ruedas, puertas,velocidad,cilindrada):
+        self.color=color
+        self.ruedas=ruedas
+        self.puertas=puertas
+        self.velocidad=velocidad
+        self.cilindrada=cilindrada
+    def __str__(self):
+        return  "color {}, {} km/h, {} ruedas, {} puertas, {} cc".format( self.color, self.velocidad, self.ruedas, self.puertas, self.cilindrada )
     
-c=coche()
-print(dir(c))
+c=coche("verde",4,5,120,6)
+print("Coche:",c)
+v=vehiculo("azul",4,4)
+print("Vehiculo:",v)

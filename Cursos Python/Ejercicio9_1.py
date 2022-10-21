97e80078d5ba6a9f53c0e679699c6f36f2ab89ed
AdalidBA <ricardo.adalidab19@gmial.com>
2022-10-21 11:41:29 -0500

    ejecicio9

--- Cursos Python/Ejercicio9_1.py
+++ Cursos Python/Ejercicio9_1.py
@@ -1,12 +1,5 @@
 paises=input("Escriba algunos paises (separados por coma): ")
-paisessplt=paises.split(',')
-paisessplt.sort()
-setpais=set()
-print(paisessplt)
-for pais in paisessplt:
-    if pais not in setpais or  not pais=='':
-        setpais.add(pais.capitalize())
-        print(setpais)
-lista=""
-print(", ".join(setpais))
+setPais=(pais for pais in paises.split(","))
+setPais.sort()
+print(", ".join(setPais))
 
